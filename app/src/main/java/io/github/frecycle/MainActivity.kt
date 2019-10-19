937047941df5e6cc6cd2fba44ad913a13c92a3e9
mertbal <mmbal@anadolu.edu.tr>
2019-10-20 02:18:50 +0300

    Second

--- app/src/main/java/io/github/frecycle/MainActivity.kt
+++ app/src/main/java/io/github/frecycle/MainActivity.kt
@@ -11,34 +11,52 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     private lateinit var auth : FirebaseAuth
+    private val email = userEmailText.text.toString()
+    private val pass = passwordText.text.toString()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         auth = FirebaseAuth.getInstance()
+
+        val currentUser = auth.currentUser
+
+        if(currentUser != null){
+            val intent = Intent(applicationContext,FeedActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 
     fun singInClicked(view : View){
-
+        auth.signInWithEmailAndPassword(email,pass).addOnCompleteListener { task->
+            if(task.isSuccessful){
+                Toast.makeText(applicationContext,"Welcome ${auth.currentUser?.email.toString()}", Toast.LENGTH_LONG).show()
+                val intent = Intent(applicationContext,FeedActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }.addOnFailureListener { exception ->
+            if(exception != null){
+                Toast.makeText(applicationContext,exception.localizedMessage.toString(),Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
     fun singUpClicked(view : View){
-        val email = userEmailText.text.toString()
-        val pass = passwordText.text.toString()
-        if (email != null && pass != null){
             auth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener { task ->
                 if(task.isSuccessful){
                     val intent = Intent(applicationContext,FeedActivity::class.java)
                     startActivity(intent)
                     finish()
-            }
+                }
             }.addOnFailureListener { exception ->
                 if(exception != null){
                     Toast.makeText(applicationContext,exception.localizedMessage.toString(),Toast.LENGTH_LONG).show()
                 }
             }
-        }
+
 
     }
 }
